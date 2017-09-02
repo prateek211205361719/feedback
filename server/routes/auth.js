54a9722e2e58523c959b932ed6d66f55fa0b8e28
prateek211205361719 <prateek@ceptes.com>
2017-09-02 13:08:00 +0530

    second commit

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,6 @@
 
 
 var passport = require("passport");
-
 module.exports = (app) => {
     app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
     app.get('/auth/google/callback',  passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
